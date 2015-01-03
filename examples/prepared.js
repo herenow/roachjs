76f93b28e267f9455a7f6c6fa165c1a08bceeab3
herenow <leonardoshiro@gmail.com>
2015-01-03 01:43:45 -0200

    prepare/flush queries example

---
+++ examples/prepared.js
@@ -0,0 +1,19 @@
+var Roach = require('../index.js')
+
+var client = new Roach({
+	host: '127.0.0.1',
+	port: 8080
+})
+
+for(var i = 0; i < 1000; i++) {
+	(function(i) {
+		var c = client.prepare()
+		c.put(i, i, function(err, res) {
+		})
+		c.get(i, function(err, res) {
+			console.log(res)
+		})
+		c.flush()
+	})(i)
+}
+
