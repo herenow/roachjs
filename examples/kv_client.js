84914851db142cb07d2d46d005da1e455033bf93
herenow <leonardoshiro@gmail.com>
2015-01-04 04:27:37 -0200

    more examples

--- examples/kv_client.js
+++ examples/kv_client.js
@@ -1,4 +1,3 @@
-
 var Roach = require('../index.js')
 
 var client = new Roach({
@@ -12,4 +11,32 @@ client.put("1", 'asd', function(err, res) {
 
 client.get("1", function(err, res) {
 	console.log('get', err, res)
+})
+
+client.increment("example_counter", 5, function(err, res) {
+	console.log('increment', err, res)
+})
+
+client.get("example_counter", function(err, res) {
+	console.log('get example_counter', err, res)
+})
+
+client.conditionalPut("1", 5, 10, function(err, res) {
+	console.log("putConditional", err, res)
+})
+
+client.scan("1", "5", function(err, res) {
+	console.log("scan", err, res)
+})
+
+client.contains("1", function(err, res) {
+	console.log("contains", err, res)
+})
+
+client.delete("1", function(err, res) {
+	console.log("delete", err, res)
+})
+
+client.deleteRange("1", "5", function(err, res) {
+	console.log("deleteRange", err, res)
 })
--- examples/prepared.js
+++ examples/prepared.js
@@ -5,15 +5,11 @@ var client = new Roach({
 	port: 8080
 })
 
-for(var i = 0; i < 10; i++) {
-	(function(i) {
-		var c = client.prepare()
-		c.put(i, i, function(err, res) {
-		})
-		c.get(i, function(err, res) {
-			console.log(res)
-		})
-		c.flush()
-	})(i)
-}
+var c = client.prepare()
+c.put(i, i, function(err, res) {
+})
+c.get(i, function(err, res) {
+	console.log(res)
+})
+c.flush()
 
